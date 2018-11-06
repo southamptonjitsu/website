49cca68c5bc696f6ffb634f58d19b05ba36ae2d5
Clark Sirl <clark.sirl@efficioconsulting.com>
2018-11-06 12:57:27 +0000

    Removed babel config

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,6 @@ module.exports = {
     },
     module: {
         rules: [
-            // {
-            //     test: /\.js$/,
-            //     exclude: /node_modules/,
-            //     use: {
-            //         loader: "babel-loader"
-                // }
-            // },
             {
                 test: /\.(css|scss)$/,
                 // use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
