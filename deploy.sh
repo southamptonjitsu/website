ab25df261a9775f25b771ed4e7b68e7954f6ecbb
Clark Sirl <click66@gmail.com>
2019-01-11 14:43:51 +0000

    Disabled host checking for copy artifact

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ DEPLOY_DIR="$RELEASES_DIR/$NOW"
 SERVED_PATH="./htdocs"
 
 echo "Copying file to remote server..."
-sshpass -p $SSH_PASS scp -r $ARTIFACT_NAME $SSH_USER@$SSH_HOST:$ARTIFACT_PATH
-
+sshpass -p $SSH_PASS -oStrictHostKeyChecking=no scp -r $ARTIFACT_NAME $SSH_USER@$SSH_HOST:$ARTIFACT_PATH
+exit 1
 echo "Starting release..."
 sshpass -p $SSH_PASS ssh $SSH_USER@$SSH_HOST "bash -s" < ./release.sh $ARTIFACT_PATH $RELEASES_DIR $DEPLOY_DIR $SERVED_PATH
