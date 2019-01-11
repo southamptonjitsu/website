a1098fd95f97658b6ae7d63e4e5bf634170e1f9c
Clark Sirl <click66@gmail.com>
2019-01-11 15:25:30 +0000

    Added build dir variable in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+TARGET_DIR=${1:-.}
+
 docker-compose build
 docker-compose -f docker-compose.yml up -d app
 docker-compose -f docker-compose.yml run composer install --no-dev
 docker-compose -f docker-compose.yml run npm run build
 docker-compose -f docker-compose.yml run app tar -zcvf app.tar.gz --files-from=buildfiles.txt
-docker cp $(docker-compose ps -q app):/var/www/html/app.tar.gz app.tar.gz
+docker cp $(docker-compose ps -q app):/var/www/html/app.tar.gz "$TARGET_DIR/app.tar.gz"
 docker-compose down -v
