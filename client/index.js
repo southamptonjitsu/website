49e7d0f017f8540a2de819844219e815b2a37d50
Clark Sirl <click66@gmail.com>
2019-05-04 12:49:19 +0100

    Removed js and css resources from build artifact
    Moved fonts dir out of dist dir to prevent accidental deletion

---
+++ client/index.js
@@ -0,0 +1,4 @@
+require('./js/app');
+
+require('./css/foundation.css');
+require('./css/main.scss');
--- index.js
+++
@@ -1,4 +0,0 @@
-require('./resources/js/app');
-
-require('./resources/css/foundation.css');
-require('./resources/css/main.scss');
